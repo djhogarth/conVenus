2071c54e4c066ea00783438c47f5aa75763e3162
DJ Hogarth <dariyhogarth@hotmail.com>
2021-11-21 16:38:22 -0500

    Added Entity Framework, DbContext class, API Controller and made code asynchronous

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int ID { get; set; } 
+
+        public string Username { get; set; }
+    }
+}
