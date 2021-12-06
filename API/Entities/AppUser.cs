a0d734cf8b4230fa86b28db86b4d8ce508f3f203
DJ Hogarth <dariyhogarth@hotmail.com>
2021-12-06 17:19:33 -0500

    Added CORS support in API,  HTTPS suport in client

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,6 +9,6 @@ namespace API.Entities
     {
         public int ID { get; set; } 
 
-        public string Username { get; set; }
+        public string UserName { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -37,10 +37,8 @@ namespace API
             });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,13 +47,14 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
