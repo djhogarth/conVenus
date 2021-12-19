cd133a2a9eaf46ef1193b1171b938d16f7a0231d
DJ Hogarth <dariyhogarth@hotmail.com>
2021-12-19 02:49:13 -0500

    Added password fields to AppUser entity

---
+++ API/Data/Migrations/20211219072156_AddedUserPasswordProperties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class AddedUserPasswordProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,5 +10,10 @@ namespace API.Entities
         public int ID { get; set; } 
 
         public string UserName { get; set; }
+        
+        public byte[] PasswordSalt {get; set; }
+
+        
+        public byte[] PasswordHash { get; set; }
     }
 }
