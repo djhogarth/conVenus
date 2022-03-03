f4779f2d5867a7c7fcb1c877cf1b4002d5cd25cc
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-03 13:37:18 -0500

    added a controller for testing our app

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+   //returns errors so we can see what we get back from various responses in our application
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+            
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetKey()
+        {
+            return "Got Key";
+        }
+
+    
+        
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if(thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+
+              
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            if(thing == null) return NotFound();
+
+            return thingToReturn;
+        }
+        
+       
+        
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("this was not a good request");
+        }
+
+               
+    }
+}
