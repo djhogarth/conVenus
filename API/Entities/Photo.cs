d9ca57134d88bdd07159e5a4b24b92dd4d467c39
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-05 00:54:39 -0500

    fully defined relationship between AppUser and Photo

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -3,11 +3,15 @@ namespace API.Entities
   public class Photo
   {
     public int Id { get; set; }
-    
+
     //photo Url
     public string Url { get; set; }
     public bool IsMainPhoto { get; set; }
     //used for the photo storage solution that's used
+
+    public AppUser User { get; set; }
+    public int UserId { get; set; }
+    
     public string PublicId { get; set; }
   }
 }
