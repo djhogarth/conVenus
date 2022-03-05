2d71aa3dfa0b4ae3e6f5f02f7e83d10f04c4986a
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-04 20:01:51 -0500

    updated AppUser entity and added photo entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,19 +5,39 @@ using System.Threading.Tasks;
 
 namespace API.Entities
 {
-    
+
     public class AppUser
     {
         // userID
-        public int ID { get; set; } 
+        public int ID { get; set; }
 
         public string UserName { get; set; }
-        
-        /*  password salts prevent two users with the same password from getting the same hash. 
+
+        /*  password salts prevent two users with the same password from getting the same hash.
             It also adds another layer of complexity to the password hash   */
         public byte[] PasswordSalt {get; set; }
 
         //password hashing is used to ecrypt passwords stored in the database
         public byte[] PasswordHash { get; set; }
+
+        public DateTime DateOfBirth{get; set;}
+
+        //The name the user wants to be known as
+        public string Alias { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public String Introduction  { get; set; }
+        public string LookingFor { get; set; }
+
+        public string Interests {get; set; }
+
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
     }
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+namespace API.Entities
+{
+  public class Photo
+  {
+    public int Id { get; set; }
+    
+    //photo Url
+    public string Url { get; set; }
+    public bool IsMainPhoto { get; set; }
+    //used for the photo storage solution that's used
+    public string PublicId { get; set; }
+  }
+}
