039296e2bbd733230e32f3934f55979532694e8d
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-11 14:16:17 -0500

    install cloudinary to our app and strongly type config settings

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
     {
     public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
