cc44b3011a71d48a819f230f1d3729fb09935a07
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-14 16:45:06 -0400

    updated RegisterDTO with the newly added fields

--- API/DataTransferObjects/RegisterDTO.cs
+++ API/DataTransferObjects/RegisterDTO.cs
@@ -8,14 +8,23 @@ namespace API.DataTransferObjects
 {
     public class RegisterDTO
     {
-        [Required]
-        public String UserName { get; set; }
+        [Required] public String UserName { get; set; }
 
-        [Required]
+        [Required] public string Alias { get; set; }
+
+        [Required] public string Gender { get; set; }
+
+        [Required] public DateTime DateOfBirth { get; set; }
+
+        [Required] public string city { get; set; }
+
+        [Required] public string country { get; set; }
+
+        [Required] 
         [StringLength(8, MinimumLength = 4)]
         public String Password { get; set; }
 
-        
-        
+
+
     }
 }
