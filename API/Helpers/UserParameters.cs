02f76436625f475a024664579ec0f91335ec0e4c
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-15 10:47:39 -0400

    added more helper classes for pagination and http extension method

---
+++ API/Helpers/UserParameters.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class UserParameters
+    {
+        private const int MaxPageSize = 50;
+
+        public int PageNumber {get; set;} = 1;
+
+        //Default Page size
+        private int _pageSize = 10;
+
+        public int PageSize{
+
+          //only allow a max page size of 50.
+          get => _pageSize;
+          set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
