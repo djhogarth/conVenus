cf455cacd1aced7a75d583ea6dbc558de9601c3d
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-15 14:52:33 -0400

    implement changed method signature and add pagination header to http response

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DataTransferObjects;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using AutoMapper;
@@ -38,8 +39,10 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers(){
-          var users = await _userRepository.GetMembersAsync();
+        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers([FromQuery] UserParameters parameters){
+          var users = await _userRepository.GetMembersAsync(parameters);
+
+          Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(users);
 
