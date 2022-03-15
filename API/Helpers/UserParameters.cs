87c96a230aa6f39e21899be98a9d1f7e717a263c
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-15 16:59:08 -0400

    add properties for filtering out matches

--- API/Helpers/UserParameters.cs
+++ API/Helpers/UserParameters.cs
@@ -20,5 +20,10 @@ namespace API.Helpers
           get => _pageSize;
           set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
+
+        public string CurrentUsername { get; set; }
+
+        public string Gender { get; set; }
+        
     }
 }
