fbea11f23561f5bb4921e2023388ff907498f594
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-17 21:24:20 -0400

    added automapper profile for Message and MessageDTO. created CreateMessageDTO

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,13 +14,25 @@ namespace API.Helpers
     public AutoMapperProfiles()
     {
       CreateMap<AppUser, MemberDTO>()
-        .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
-        .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+        .ForMember(dest => dest.PhotoUrl,
+          opt => opt.MapFrom(src =>
+            src.Photos.FirstOrDefault(x =>
+              x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                  opt.MapFrom(src =>
+                    src.DateOfBirth.CalculateAge()));
 
       CreateMap<Photo, PhotoDTO>();
 
       CreateMap<MemberUpdateDTO, AppUser>();
       CreateMap<RegisterDTO, AppUser>();
+      CreateMap<Message, MessageDTO>()
+        .ForMember(dest => dest.SenderPhotoUrl, opt =>
+          opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x =>
+            x.IsMain).Url))
+        .ForMember(dest => dest.RecipientPhotoUrl, opt =>
+          opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(x =>
+            x.IsMain).Url));
     }
   }
 }
