1b2b1a8308c95b5c9bdad7d1af9c5d21113ca6db
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-19 09:19:59 -0400

    implemented loadMessages(), and pageChanged()

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from 'ngx-bootstrap/pagination';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
-@Component({
+@Component(
+  {
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
-})
+  })
+
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    this.loadMessages();
   }
+
+  loadMessages()
+  {
+    this.messageService.getMessages(this.pageNumber,
+      this.pageSize, this.container).subscribe(
+        response => {
+          this.messages = response.result;
+          this.pagination = response.pagination;
+        });
+  }
+
+  pageChanged(event: any)
+  {
+    if(this.pageNumber !== event.page)
+    {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+
+  }
+
+
 
 }
