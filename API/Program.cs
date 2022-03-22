4313d52273449beae210ec09a98a6662beda492d
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-22 15:15:59 -0400

    added new migration, updated seed data generating to work with Identity as well as program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,8 +29,9 @@ namespace API
               /* we want to get our DataContext service so we can pass it
               into our Seed method */
               var context = services.GetRequiredService<DataContext>();
+              var userManager = services.GetRequiredService<UserManager<AppUser>>();
               await context.Database.MigrateAsync();
-              await Seed.SeedUsers(context);
+              await Seed.SeedUsers(userManager);
             }
             catch(Exception ex)
             {
