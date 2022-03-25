83897bf1f4dcc5041ea70862576132d9bbc79411
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-25 08:48:00 -0400

    Created and implemented new entities: Group and Connection. The plan is to use the database to track who sees what messages (read/unread function).

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -21,6 +21,10 @@ namespace API.Data
         public DbSet<AppUserLike> Likes { get; set; }
         public DbSet<Message> Message { get; set; }
 
+        public DbSet<Group> Groups {get; set;}
+
+        public DbSet<Connection> Connections { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
 
