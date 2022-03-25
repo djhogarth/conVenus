f2f30a90b3879314ec799cd52042659a809b3309
DJ Hogarth <dariyhogarth@hotmail.com>
2022-03-25 13:26:43 -0400

    Attach a 'Z' at the end of DateTIme so the client knows it's UTC and can make the local time conversion.

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -33,6 +33,7 @@ namespace API.Helpers
         .ForMember(dest => dest.RecipientPhotoUrl, opt =>
           opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(x =>
             x.IsMain).Url));
+      CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
     }
   }
 }
